e29b054b894d5ccd8b22833043af60ae00a2cf62
Milosz <miloszloniewski02@gmail.com>
2023-03-23 10:22:51 +0100

    finished challenge

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+import { data } from './data.js';
+
+const handleData = () => {
+	const summaryItems = document.querySelectorAll('.app__summary-list-item');
+
+	for (let i = 0; i < data.length; i++) {
+		const img = document.createElement('img');
+		const paragraph = document.createElement('p');
+		const span = document.createElement('span');
+
+		img.setAttribute('src', data[i].icon);
+		img.setAttribute('alt', ' ');
+
+		span.innerHTML = data[i].score;
+
+		paragraph.appendChild(span);
+		paragraph.innerHTML += ' / 100';
+
+		summaryItems[i].appendChild(img);
+		summaryItems[i].append(data[i].category);
+		summaryItems[i].appendChild(paragraph);
+	}
+};
+
+handleData();
